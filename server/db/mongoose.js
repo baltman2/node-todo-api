4b7a6e0c136d30c3baa89a2e0c215a93baf26bf2
Hongshen2010 <yuhongshen2010@gmail.com>
2017-12-28 17:27:02 -0500

    add test env and test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
-const host_url = 'mongodb://localhost:27017/';
-
 mongoose.Promise = global.Promise;
-mongoose.connect(host_url);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
