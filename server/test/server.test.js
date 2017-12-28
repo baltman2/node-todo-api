e4b6108fb3c0d123d5b5194cef5c2a4d810b91e9
Hongshen2010 <yuhongshen2010@gmail.com>
2017-12-28 16:25:34 -0500

    testing matcher bug fixed

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -130,7 +130,7 @@ describe('PATCH /todos/:id', () => {
       .expect(res => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
-        // expect(typeof res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.completedAt).toBe(null);
       })
       .end(done);
   });
