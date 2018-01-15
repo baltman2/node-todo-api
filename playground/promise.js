350a0223d251947abfc69f13551cdc1da98acf12
Hongshen2010 <yuhongshen2010@gmail.com>
2018-01-14 19:06:35 -0500

    add debugger

--- playground/promise.js
+++ playground/promise.js
@@ -1,12 +1,72 @@
 // import { setTimeout } from "timers";
 // const setTimeout = require('timers');
 
-var somePromise = new Promise((resolve, reject) => {
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('it worked');
+//   }, 1000);
+// });
+
+// somePromise.then((message) => {
+//   console.log('resolved', message);
+// });
+
+function doSth() {
   setTimeout(() => {
-    resolve('it worked');
+    console.log('doSth finished');
   }, 1000);
+}
+function doSthElse() {
+  setTimeout(() => {
+    console.log('doSthElse finished');
+  }, 2000);
+}
+function doSthFinal() {
+  setTimeout(() => {
+    console.log('doSthFinal finished');
+  }, 3000);
+}
+function failureCb(err) {
+  console.log(err);
+}
+
+const say = str => { console.log(str) };
+const wait = ms => new Promise(resolve => {
+  setTimeout(resolve, ms);
 });
 
-somePromise.then((message) => {
-  console.log('resolved', message);
+// wait(1000).then(() => say('2 secs')).catch(e => { console.log(e) });
+
+// const arrFunc = args => args;
+// console.log(arrFunc('dsfsf'));
+
+var materials = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+]; 
+var lengths = materials.map(material => material.length);
+// console.log(lengths);
+
+let myPromise = new Promise(function(resolve, reject){
+  setTimeout(function() {
+    resolve('success');
+  }, 2000);
 });
+myPromise.then(function(mesg){
+  console.log(mesg);
+});
+
+// Node doesn't support async/await.
+// void async function foo() {
+//   try {
+//     let result = await doSth();
+//     let newResult = await doSthElse();
+//     let finalResult = await doSthFinal();
+//     console.log(`Got the final result ${finalResult}`);
+//   } catch(err) {
+//     failureCb(err);
+//   }
+// }();
+
